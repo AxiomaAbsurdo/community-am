9d1b5bf8ad3fa0e359176a4c0600ad564ce792c1
AxiomaAbsurdo <m.mortara01@gmail.com>
2023-06-14 19:13:56 -0300

    fix: next config for gh action next deploy

--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {}
 
-module.exports = nextConfig
+module.exports = {
+    exportPathMap: function () {
+      return {
+        '/': { page: '/' },
+        // Add other routes if needed
+      };
+    },
+    output: 'export',
+  };
+  
