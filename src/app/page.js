676dace4a3e3a945260f479a2648e7c54a671e4b
matias.mortara <matias.mortara@dualbootpartners.com>
2023-08-03 16:40:14 -0300

    feature: add google analitycs and SEO tags

--- src/app/page.js
+++ src/app/page.js
@@ -4,46 +4,70 @@ import styles from "../app/page.module.css";
 
 const Home = () => {
   return (
-    <div className={styles.container}>
-      <div className={styles.imageContainer}>
-        <img
-          src="profile_image_am_2.jpeg"
-          alt="Profile"
-          className={styles.profileImage}
+    <><div>
+      <Head>
+        <title>Cummunity AM</title>
+        <meta name="description" content="Master Marketing - Offering expert tips and insights for Marketing Analysts, Social Media Managers, Copywriters, and all digital marketing enthusiasts." />
+        <meta name="keywords" content="Marketing Analyst, Social Media Manager, Copywriting, Branding Tips, Digital Marketing, Marketing Blog" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="robots" content="index, follow" />
+        <meta charSet="UTF-8" />
+
+        {/* Global Site Tag (gtag.js) - Google Analytics */}
+        <script
+          async src="https://www.googletagmanager.com/gtag/js?id=G-CSBKCD60YL"
         />
-      </div>
-      <div className={styles.linksContainer}>
-        <nav>
-          <ul className={styles.linksList}>
-            <li>
-              <Link href="https://www.instagram.com/community_am/">
-                <FaInstagram /> Instagram
-              </Link>
-            </li>
-            <li>
-              <Link href="https://www.linkedin.com/in/avmartinez/">
-                <FaLinkedin /> LinkedIn
-              </Link>
-            </li>
-            <li>
-              <Link href="https://www.facebook.com/profile.php?id=100093287414365">
-                <FaFacebook /> Facebook
-              </Link>
-            </li>
-            <li>
-              <Link href="https://drive.google.com/file/d/10-_scx_Llddhp4k7a3buYEkFmyBdBsTI/view?usp=sharing">
-                <FaFileAlt /> Guía Emprendedores
-              </Link>
-            </li>
-            <li>
-              <Link href="https://calendly.com/community_am/15min">
-                <FaCalendarAlt /> Agendá una Asesoría
-              </Link>
-            </li>
-          </ul>
-        </nav>
-      </div>
-    </div>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+
+                gtag('config', 'G-CSBKCD60YL');;
+              `,
+          }}
+        />
+      </Head>
+    </div><div className={styles.container}>
+        <div className={styles.imageContainer}>
+          <img
+            src="profile_image_am_2.jpeg"
+            alt="Profile"
+            className={styles.profileImage} />
+        </div>
+        <div className={styles.linksContainer}>
+          <nav>
+            <ul className={styles.linksList}>
+              <li>
+                <Link href="https://www.instagram.com/community_am/">
+                  <FaInstagram /> Instagram
+                </Link>
+              </li>
+              <li>
+                <Link href="https://www.linkedin.com/in/avmartinez/">
+                  <FaLinkedin /> LinkedIn
+                </Link>
+              </li>
+              <li>
+                <Link href="https://www.facebook.com/profile.php?id=100093287414365">
+                  <FaFacebook /> Facebook
+                </Link>
+              </li>
+              <li>
+                <Link href="https://drive.google.com/file/d/10-_scx_Llddhp4k7a3buYEkFmyBdBsTI/view?usp=sharing">
+                  <FaFileAlt /> Guía Emprendedores
+                </Link>
+              </li>
+              <li>
+                <Link href="https://calendly.com/community_am/15min">
+                  <FaCalendarAlt /> Agendá una Asesoría
+                </Link>
+              </li>
+            </ul>
+          </nav>
+        </div>
+      </div></>
   );
 };
 
