f30c8ad03c33f8ca02e847ad66329b3d36fa47e0
Matias Mortara <m.mortara01@gmail.com>
2023-08-06 14:42:51 +0000

    fix  image tag for nextjs

--- src/app/page.js
+++ src/app/page.js
@@ -6,44 +6,44 @@ import styles from "../app/page.module.css";
 const Home = () => {
   return (
     <div className={styles.container}>
-        <div className={styles.imageContainer}>
-          <img
-            src="profile_image_am_2.jpeg"
-            alt="Profile"
-            className={styles.profileImage} />
-        </div>
-        <div className={styles.linksContainer}>
-          <nav>
-            <ul className={styles.linksList}>
-              <li>
-                <Link href="https://www.instagram.com/community_am/">
-                  <FaInstagram /> Instagram
-                </Link>
-              </li>
-              <li>
-                <Link href="https://www.linkedin.com/in/avmartinez/">
-                  <FaLinkedin /> LinkedIn
-                </Link>
-              </li>
-              <li>
-                <Link href="https://www.facebook.com/profile.php?id=100093287414365">
-                  <FaFacebook /> Facebook
-                </Link>
-              </li>
-              <li>
-                <Link href="https://drive.google.com/file/d/10-_scx_Llddhp4k7a3buYEkFmyBdBsTI/view?usp=sharing">
-                  <FaFileAlt /> Guía Emprendedores
-                </Link>
-              </li>
-              <li>
-                <Link href="https://calendly.com/community_am/15min">
-                  <FaCalendarAlt /> Agendá una Asesoría
-                </Link>
-              </li>
-            </ul>
-          </nav>
-        </div>
+      <div className={styles.imageContainer}>
+        <Image
+          src="profile_image_am_2.jpeg"
+          alt="Profile"
+        />
       </div>
+      <div className={styles.linksContainer}>
+        <nav>
+          <ul className={styles.linksList}>
+            <li>
+              <Link href="https://www.instagram.com/community_am/">
+                <FaInstagram /> Instagram
+              </Link>
+            </li>
+            <li>
+              <Link href="https://www.linkedin.com/in/avmartinez/">
+                <FaLinkedin /> LinkedIn
+              </Link>
+            </li>
+            <li>
+              <Link href="https://www.facebook.com/profile.php?id=100093287414365">
+                <FaFacebook /> Facebook
+              </Link>
+            </li>
+            <li>
+              <Link href="https://drive.google.com/file/d/10-_scx_Llddhp4k7a3buYEkFmyBdBsTI/view?usp=sharing">
+                <FaFileAlt /> Guía Emprendedores
+              </Link>
+            </li>
+            <li>
+              <Link href="https://calendly.com/community_am/15min">
+                <FaCalendarAlt /> Agendá una Asesoría
+              </Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </div>
   );
 };
 
