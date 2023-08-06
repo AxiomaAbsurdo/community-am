f80117790186c57fa123de16606d1286e5553440
Matias Mortara <m.mortara01@gmail.com>
2023-08-06 16:03:41 +0000

    fix: update profile images and chage style

--- src/app/page.js
+++ src/app/page.js
@@ -9,14 +9,14 @@ const Home = () => {
     <div className={styles.container}>
       <div className={styles.imageContainer}>
         <Image
-          src="profile_image_am_2.jpeg"
+          src="profile_image_am_1.jpeg"
           alt="Profile"
           className="object-cover"
+          layout="fill"
+          objectFit="cover"
           style={{
             width: 100%,
             height: 100%,
-            object-fit: cover,
-            border-radius: 50%,
           }}
         />
       </div>
